87a794d2b0dbc391e87193ad214703b02d5d26ed
Erlik-1417 <kayradogru1417@gmail.com>
2026-07-14 20:44:35 +0300

    vercel

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/goit-react-hw-01/', // https://github.com/tercihtlcom-hue/goit-react-hw-01
+  base: '/',
 })
